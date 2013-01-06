6f6342b7576b349ac7ec80cc9b5517ec64d9ee2f
carolina <carolina@sindominio.net>
2013-01-06 17:38:05 +0100

    Added dropdown cycle list and modified factories

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 include Warden::Test::Helpers
 
   describe "GET /events" do
+    before(:each) do
+     FactoryGirl.create_list(:event,8)
+    end
+
     it "display events index" do
-      FactoryGirl.create(:event)
       visit events_path
       page.should have_content("Event 1")
+      page.should have_content("Date")
+      page.should have_content("Time")
       page.should have_content("Description")
       page.should have_content("Cycle")
-#      save_and_open_page
+      save_and_open_page
     end
   describe "POST /events" do
     before(:each) do
@@ -17,17 +22,20 @@ include Warden::Test::Helpers
    end
 
     it "Create an event" do
-      FactoryGirl.create(:event)
+     FactoryGirl.create_list(:cycle, 3) 
+#      FactoryGirl.create(:event)
       visit events_path
       click_link "New Event"
+      save_and_open_page
       fill_in "event_title", :with  => "Conference"
       fill_in "event_description", :with  => "Conference about"
       select '2013', :from => "event_date_1i" 
       select 'May', :from => "event_date_2i" 
       select '21', :from => "event_date_3i" 
+      select 'Cycle 2', :from => "event_cycle_id"
       click_button "Create Event"
       page.should have_content("Event was successfully created.")
-#      save_and_open_page
+
 
     end
     it "Update an event" do
@@ -41,7 +49,7 @@ include Warden::Test::Helpers
       select '2', :from => "event_date_3i" 
       click_button "Update Event"
       page.should have_content("Event was successfully updated.")
-      save_and_open_page
+ #     save_and_open_page
     end
 
   end
