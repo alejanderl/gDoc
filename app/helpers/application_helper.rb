f2f0785b77053dfba0f71e6b4a7263ad65dc781b
alejanderl <janderl@gmail.com>
2013-02-18 20:20:37 +0100

    Some extra helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def edit_button var
+    render "shared/buttons/edit_button", :t => var    
+  end
+
 end
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,13 @@
 module TagsHelper
+  
+  def tags_autocomplete(var)
+    
+   
+      render("tags/tags_autocomplete", :f => var ) 
+      #("favourites/favourite", :t => var)
+
+    
+  end
+    
+
 end
